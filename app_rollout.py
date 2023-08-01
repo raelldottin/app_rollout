270eec1734aa6ad95382b3749778e0c344ff7b17
Raell Dottin <raell.dottin@gmail.com>
2023-08-01 02:23:10 -0400

    add test for mobile smart group creation

--- app_rollout.py
+++ app_rollout.py
@@ -24,7 +24,7 @@ log_capture_string = io.StringIO()
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
+    format="%(asctime)s [%(levelname)s] %(funcName)s() - %(message)s",
     handlers=[
         logging.FileHandler(logfilepath),
         logging.StreamHandler(sys.stdout),
@@ -104,13 +104,12 @@ def get_mobile_app(
     mobiledeviceapps = api.get("mobiledeviceapplications")
 
     try:
-        if mobiledeviceapps:
-            for app in mobiledeviceapps["mobile_device_applications"][
-                "mobile_device_application"
-            ]:
-                if appname == app["name"]:
-                    logging.info(f"Found Mobile Device App entry for {app['name']}.")
-                    return app["id"], app["name"], app["version"], app["bundle_id"]
+        for app in mobiledeviceapps["mobile_device_applications"][
+            "mobile_device_application"
+        ]:
+            if appname == app["name"]:
+                logging.info(f"Found Mobile Device App entry for {app['name']}.")
+                return app["id"], app["name"], app["version"], app["bundle_id"]
     except:
         logging.exception("Unable to get list of mobile device apps", exc_info=True)
 
@@ -134,11 +133,12 @@ def get_mobile_device_group(groupname: str) -> tuple[str, str] | int:
             if (not availableId) or (availableId < int(group["id"])):
                 availableId = int(group["id"])
             if groupname == group["name"]:
+                logging.warning(f"{group=}")
                 return group["id"], group["name"]
-
     except:
         logging.exception("Unable to get list of mobile device groups.", exc_info=True)
 
+    logging.info(f"Unable to find mobile device group: {groupname}")
     return availableId + 1
 
 
@@ -164,13 +164,16 @@ def set_mobile_app_details(id):
 
     do we have to convert dictionary to xml?
     can the library perform a put with a dictionary?
-    {'mobile_device_application': {'general': {'id': '92', 'name': 'NewStore Associate App', 'display_name': 'NewStore Associate App', 'description': 'The NewStore Associate App is for a retail store associate to perform various tasks in-store. Some of the key transactions performed by the store associate using the app are mentioned below.', 'bundle_id': 'com.newstore.associate-one', 'version': '1.36.0', 'internal_app': 'true', 'category': {'id': '2', 'name': 'iOS Applications'}, 'ipa': {'name': None, 'uri': None, 'data': None}, 'icon': {'id': '10837', 'name': '1024x1024bb.png', 'uri': 'https://ics.services.jamfcloud.com/icon/hash_becc2d03a7df883dd90054391441b13907a5385ae3f0ffa425b1f84007dfe1c4'}, 'mobile_device_provisioning_profile': None, 'url': 'https://apps.apple.com/us/app/newstore-associate-app/id1545162024', 'itunes_store_url': 'https://apps.apple.com/us/app/newstore-associate-app/id1545162024', 'make_available_after_install': 'true', 'itunes_country_region': 'US', 'itunes_sync_time': '25200', 'deployment_type': 'Install Automatically/Prompt Users to Install', 'deploy_automatically': 'true', 'deploy_as_managed_app': 'true', 'remove_app_when_mdm_profile_is_removed': 'true', 'prevent_backup_of_app_data': 'false', 'allow_user_to_delete': 'true', 'require_network_tethered': 'false', 'keep_description_and_icon_up_to_date': 'true', 'keep_app_updated_on_devices': 'false', 'free': 'true', 'take_over_management': 'true', 'host_externally': 'true', 'external_url': 'https://apps.apple.com/us/app/newstore-associate-app/id1545162024', 'site': {'id': '-1', 'name': 'None'}}, 'scope': {'all_mobile_devices': 'true', 'all_jss_users': 'true', 'mobile_devices': None, 'buildings': None, 'departments': None, 'mobile_device_groups': None, 'jss_users': None, 'jss_user_groups': None, 'limitations': {'users': None, 'user_groups': None, 'network_segments': None}, 'exclusions': {'mobile_devices': {'mobile_device': {'id': '412', 'name': 'Joe Kabashi (Quality Assurance) iPad', 'udid': '00008030-001C18A60E88C02E', 'wifi_mac_address': '9C:3E:53:C3:CC:CD'}}, 'buildings': {'building': [{'id': '1', 'name': 'HQ'}, {'id': '77', 'name': 'Satellite Office  - Chicago'}, {'id': '93', 'name': 'UNTUCKit  Westfield'}, {'id': '14', 'name': 'UNTUCKit Boston'}, {'id': '3', 'name': 'UNTUCKit Brookfield'}, {'id': '87', 'name': 'UNTUCKit Yorkdale'}]}, 'departments': None, 'mobile_device_groups': None, 'users': None, 'user_groups': None, 'network_segments': None, 'jss_users': None, 'jss_user_groups': None}}, 'self_service': {'self_service_install_button_text': 'Install', 'self_service_after_install_button_text': 'Reinstall', 'self_service_description': 'The NewStore Associate App is for a retail store associate to perform various tasks in-store. Some of the key transactions performed by the store associate using the app are mentioned below.', 'self_service_icon': {'id': '10837', 'filename': '1024x1024bb.png', 'uri': 'https://ics.services.jamfcloud.com/icon/hash_becc2d03a7df883dd90054391441b13907a5385ae3f0ffa425b1f84007dfe1c4'}, 'feature_on_main_page': 'false', 'self_service_categories': {'category': {'id': '2', 'name': 'iOS Applications', 'display_in': 'true'}}, 'notification': 'false', 'notification_subject': None, 'notification_message': None}, 'vpp_codes': None, 'vpp': {'assign_vpp_device_based_licenses': 'true', 'vpp_admin_account_id': '1', 'total_vpp_licenses': '1000', 'remaining_vpp_licenses': '606', 'used_vpp_licenses': '394'}, 'app_configuration': {'preferences': '<dict>\r\n    <key>live</key>\r\n    <string>untuckit</string>\r\n    <key>test</key>\r\n    <string>untuckit-staging</string>\r\n</dict>'}}}
+    {'mobile_device_application': {'general': {'id': '92', 'name': 'NewStore Associate App', 'display_name': 'NewStore Associate App', 'description': 'The NewStore Associate App is for a retail store associate to perform various tasks in-store. Some of the key transactions performed by the store associate using the app are mentioned below.', 'bundle_id': 'com.newstore.associate-one', 'version': '1.36.0', 'internal_app': 'true', 'category': {'id': '2', 'name': 'iOS Applications'}, 'ipa': {'name': None, 'uri': None, 'data': None}, 'icon': {'id': '10837', 'name': '1024x1024bb.png', 'uri': 'https://ics.services.jamfcloud.com/icon/hash_becc2d03a7df883dd90054391441b13907a5385ae3f0ffa425b1f84007dfe1c4'}, 'mobile_device_provisioning_profile': None, 'url': 'https://apps.apple.com/us/app/newstore-associate-app/id1545162024', 'itunes_store_url': 'https://apps.apple.com/us/app/newstore-associate-app/id1545162024', 'make_available_after_install': 'true', 'itunes_country_region': 'US', 'itunes_sync_time': '25200', 'deployment_type': 'Install Automatically/Prompt Users to Install', 'deploy_automatically': 'true', 'deploy_as_managed_app': 'true', 'remove_app_when_mdm_profile_is_removed': 'true', 'prevent_backup_of_app_data': 'false', 'allow_user_to_delete': 'true', 'require_network_tethered': 'false', 'keep_description_and_icon_up_to_date': 'true', 'keep_app_updated_on_devices': 'false', 'free': 'true', 'take_over_management': 'true', 'host_externally': 'true', 'external_url': 'https://apps.apple.com/us/app/newstore-associate-app/id1545162024', 'site': {'id': '-1', 'name': 'None'}}, 'scope': {'all_mobile_devices': 'true', 'all_jss_users': 'true', 'mobile_devices': None, 'buildings': None, 'departments': None, 'mobile_device_groups': None, 'jss_users': None, 'jss_user_groups': None, 'limitations': {'users': None,
+        'user_groups': None, 'network_segments': None}, 'exclusions': {'mobile_devices': {'mobile_device': {'id': '412', 'name': 'Joe Kabashi (Quality Assurance) iPad', 'udid': '00008030-001C18A60E88C02E', 'wifi_mac_address': '9C:3E:53:C3:CC:CD'}}, 'buildings': {'building': [{'id': '1', 'name': 'HQ'}, {'id': '77', 'name': 'Satellite Office  - Chicago'}, {'id': '93', 'name': 'UNTUCKit  Westfield'}, {'id': '14', 'name': 'UNTUCKit Boston'}, {'id': '3', 'name': 'UNTUCKit Brookfield'}, {'id': '87', 'name': 'UNTUCKit Yorkdale'}]}, 'departments': None, 'mobile_device_groups': None, 'users': None, 'user_groups': None, 'network_segments': None, 'jss_users': None, 'jss_user_groups': None}}, 'self_service': {'self_service_install_button_text': 'Install', 'self_service_after_install_button_text': 'Reinstall', 'self_service_description': 'The NewStore Associate App is for a retail store associate to perform various tasks in-store. Some of the key transactions performed by the store associate using the app are mentioned below.', 'self_service_icon': {'id': '10837', 'filename': '1024x1024bb.png', 'uri': 'https://ics.services.jamfcloud.com/icon/hash_becc2d03a7df883dd90054391441b13907a5385ae3f0ffa425b1f84007dfe1c4'}, 'feature_on_main_page': 'false', 'self_service_categories': {'category': {'id': '2', 'name': 'iOS Applications', 'display_in': 'true'}}, 'notification': 'false', 'notification_subject': None, 'notification_message': None}, 'vpp_codes': None, 'vpp': {'assign_vpp_device_based_licenses': 'true', 'vpp_admin_account_id': '1', 'total_vpp_licenses': '1000', 'remaining_vpp_licenses': '606', 'used_vpp_licenses': '394'}, 'app_configuration': {'preferences': '<dict>\r\n    <key>live</key>\r\n    <string>untuckit</string>\r\n    <key>test</key>\r\n    <string>untuckit-staging</string>\r\n</dict>'}}}
     """
 
     return None
 
 
 def main():
+    appDetails = dict
+    deployGroup = None
     parser = argparse.ArgumentParser(
         description="Automate iOS app deployment using Jamf Pro"
     )
@@ -236,21 +239,33 @@ def main():
 
     if args.debug:
         logging.getLogger().setLevel(logging.DEBUG)
+        logging.debug("Debug logging enabled.")
 
     id, appname, appversion, bundleidentifier = get_mobile_app(args.appname)
-    print(f"{id=}, {appname=}, {appversion=}, {bundleidentifier=}")
+    logging.debug(f"{id=}, {appname=}, {appversion=}, {bundleidentifier=}")
+    if args.appversion:
+        appversion = args.appversion
 
     if id:
-        print(get_mobile_app_details(id))
+        appDetails = get_mobile_app_details(id)
+
     if appname and appversion:
-        print(get_mobile_device_group(f"{appname} Deployment Group {appversion}"))
+        deployGroup = get_mobile_device_group(
+            f"{appname} Deployment Group {appversion}"
+        )
+        logging.debug(f"{deployGroup=}")
+
+    if type(deployGroup) == int:
+        logging.debug("Create a new mobile group for deployment")
+    else:
+        logging.debug("Continue with the deployment")
 
     """
     Deployment process add deployment group as an exclusion to the mobile app entry
     Wait 5 minutes
     Remove the deployment group from the mobile app exception
 
-    we need 
+    we need
     set_mobile_app_details(id)
     sleep(300)
     set_mobile_app_details(id)
